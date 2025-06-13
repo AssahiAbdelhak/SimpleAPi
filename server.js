bcc829d8afa71937b4b8e1835ada274eaa662a3f
AssahiAbdelhak <bdalhqa89@gmail.com>
2025-06-13 21:26:47 +0200

    update

--- server.js
+++ server.js
@@ -5,19 +5,25 @@ import { addPerson, readPersons, removePerson, updatePerson } from './functions/
 const app = Fastify();
 
 app.get('/persons',async (req,res) => {
+    console.log('Received GET request for persons');
     await readPersons(req,res);
 })
 
 app.post('/persons',async (req,res) => {
+    console.log('Received POST request to add a person');
     await addPerson(req,res);
 })
 
 app.delete('/persons',async (req,res) => {
+    console.log('Received DELETE request to remove a person');
     await removePerson(req,res);
 })
 
 app.put('/persons',async (req,res) => {
+    console.log('Received PUT request to update a person');
     await updatePerson(req,res);
 })
 
-app.listen({port:3000})
+app.listen({port:3000}, () => {
+    console.log('Server is running on port 3000');
+})
