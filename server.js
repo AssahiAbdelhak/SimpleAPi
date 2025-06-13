f64a86e1f6c1f429799021799a6cfb9861023ae7
AssahiAbdelhak <bdalhqa89@gmail.com>
2025-06-13 22:37:47 +0200

    update

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.put('/persons',async (req,res) => {
     await updatePerson(req,res);
 })
 
-app.listen({port:3000}, () => {
+app.listen({port:3000,host: '0.0.0.0'}, () => {
     console.log('Server is running on port 3000');
 })
